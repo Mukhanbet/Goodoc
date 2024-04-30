38579b6c4959d1cf45a7ce9015860efae46447ea
Nurmukhanbet <nurmuhanbet2004@icloud.com>
2024-05-01 01:42:09 +0600

    Initial code of the project

---
+++ src/test/java/com/example/goodoc/GoodocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goodoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
