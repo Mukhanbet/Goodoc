1d2e035f42727c3de4d411562b7b500cb3835ecc
Nurmukhanbet <nurmuhanbet2004@icloud.com>
2024-05-01 21:13:54 +0600

    Fixed

--- src/test/java/com/example/goodoc/GoodocApplicationTests.java
+++ src/test/java/com/example/goodoc/GoodocApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.goodoc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class GoodocApplicationTests {
 
-//	@Test
+	@Test
 	void contextLoads() {
 	}
 
